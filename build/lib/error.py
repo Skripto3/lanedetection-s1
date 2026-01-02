171f161d86e60592b9d9b8a6e63db0d1ed77c980
Skripto3 <109223155+Skripto3@users.noreply.github.com>
2026-01-02 17:33:16 +0100

    Delete build/lib directory

--- build/lib/error.py
+++
@@ -1,5 +0,0 @@
-
-def input_error(message):
-    log_message = f"ERROR: {message}, expected format: python test.py <input_video_path> [<output_video_path>]"
-    print(log_message)
-
--- build/lib/roiDetection.py
+++
@@ -1,7 +0,0 @@
-#Dummy class to test
-def region_of_interest(frame):
-    #obere hälfte des bildes schwarzen
-    roi_frame = frame.copy()
-    roi_frame[:int(roi_frame.shape[0] / 3), :] = 0
-
-    return roi_frame
